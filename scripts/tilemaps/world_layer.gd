f07f3a90e1dd95b6bd9ea47a28c2bcc9b2afbe17
Mohc Bebra <danilasniper66@gmail.com>
2025-07-25 23:02:50 +0500

    dual-grid patch1

--- scripts/tilemaps/world_layer.gd
+++ scripts/tilemaps/world_layer.gd
@@ -19,15 +19,19 @@ extends TileMapLayer
 											[1,0,0,0] : Vector2i(3,3),}
 
 func _ready():
-	var m_grass_cells := self.get_used_cells_by_id(0, Vector2i(0,0), 0)
-	var h_grass_cells := self.get_used_cells_by_id(0, Vector2i(1,0), 0)
-	var l_grass_cells := self.get_used_cells_by_id(0, Vector2i(2,0), 0)
+	var l_grass_cells := self.get_used_cells_by_id(0, Vector2i(0,0), 0)
+	var m_grass_cells := self.get_used_cells_by_id(0, Vector2i(1,0), 0)
+	var h_grass_cells := self.get_used_cells_by_id(0, Vector2i(2,0), 0)
+	if l_grass_cells != []:
+		for cell_coord in l_grass_cells:
+			place_visual_tile(cell_coord, visual_layers[0], Vector2i(0,0))
 	if m_grass_cells != []:
 		for cell_coord in m_grass_cells:
-			place_visual_tile(cell_coord, visual_layers[0], Vector2i(0,0))
+			place_visual_tile(cell_coord, visual_layers[1], Vector2i(1,0))
 	if h_grass_cells != []:
 		for cell_coord in h_grass_cells:
-			place_visual_tile(cell_coord, visual_layers[1], Vector2i(1,0))
+			place_visual_tile(cell_coord, visual_layers[2], Vector2i(2,0))
+	self.clear()
 
 func place_visual_tile(cell_coord: Vector2i, visual_layer: TileMapLayer, world_tile_atlas_coord: Vector2i):
 	for x: int in range(2):
@@ -43,7 +47,7 @@ func get_tile_quarters(coord: Vector2i, world_tile_atlas_coord):
 	for x: int in range(2):
 		for y: int in range(2):
 			neighbor = self.get_cell_atlas_coords(coord - Vector2i(1,1) + Vector2i(x,y))
-			if neighbor == world_tile_atlas_coord:
+			if (neighbor == world_tile_atlas_coord) or (neighbor == world_tile_atlas_coord + Vector2i(1,0)):
 				quarters[i] = 1
 			else:
 				quarters[i] = 0
