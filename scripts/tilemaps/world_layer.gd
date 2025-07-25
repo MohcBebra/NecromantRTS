a24ace473c2c0bf8c7443d1bad00fb6b271865ad
Mohc Bebra <danilasniper66@gmail.com>
2025-07-25 18:44:04 +0500

    dual-grid(full) работает

--- scripts/tilemaps/world_layer.gd
+++ scripts/tilemaps/world_layer.gd
@@ -1,6 +1,6 @@
 extends TileMapLayer
 
-@onready var HGrassLayer := $HighGrassLayer
+@onready var visual_layers: Array[Node] = self.get_children()
 
 @onready var quarters_to_tile_source: Dictionary = {[0,1,0,0] : Vector2i(0,0),
 											[0,0,1,1] : Vector2i(1,0),
@@ -19,25 +19,31 @@ extends TileMapLayer
 											[1,0,0,0] : Vector2i(3,3),}
 
 func _ready():
-	var h_grass_cells := get_used_cells_by_id(0, Vector2i(0,0), 0)
-	for cell_coord in h_grass_cells:
-		placeVisualTile(cell_coord)
+	var m_grass_cells := self.get_used_cells_by_id(0, Vector2i(0,0), 0)
+	var h_grass_cells := self.get_used_cells_by_id(0, Vector2i(1,0), 0)
+	var l_grass_cells := self.get_used_cells_by_id(0, Vector2i(2,0), 0)
+	if m_grass_cells != []:
+		for cell_coord in m_grass_cells:
+			place_visual_tile(cell_coord, visual_layers[0], Vector2i(0,0))
+	if h_grass_cells != []:
+		for cell_coord in h_grass_cells:
+			place_visual_tile(cell_coord, visual_layers[1], Vector2i(1,0))
 
-func placeVisualTile(cell_coord):
+func place_visual_tile(cell_coord: Vector2i, visual_layer: TileMapLayer, world_tile_atlas_coord: Vector2i):
 	for x: int in range(2):
 		for y: int in range(2):
-			var quarters: Array = getTileQuarters(cell_coord + Vector2i(x,y))
+			var quarters: Array = get_tile_quarters(cell_coord + Vector2i(x,y), world_tile_atlas_coord)
 			var atlas_coord: Vector2i = quarters_to_tile_source.get(quarters)
-			$HighGrassLayer.set_cell(cell_coord + Vector2i(x,y), 1, atlas_coord, 0)
+			visual_layer.set_cell(cell_coord + Vector2i(x,y), 0, atlas_coord, 0)
 
-func getTileQuarters(coord):
+func get_tile_quarters(coord: Vector2i, world_tile_atlas_coord):
 	var quarters := [0,0,0,0]
 	var i := 0
 	var neighbor := Vector2i(0,0)
 	for x: int in range(2):
 		for y: int in range(2):
-			neighbor = get_cell_atlas_coords(coord - Vector2i(1,1) + Vector2i(x,y))
-			if neighbor != Vector2i(-1,-1):
+			neighbor = self.get_cell_atlas_coords(coord - Vector2i(1,1) + Vector2i(x,y))
+			if neighbor == world_tile_atlas_coord:
 				quarters[i] = 1
 			else:
 				quarters[i] = 0
